4ea56a3df9ccd482279cf7c1591e761399616c56
mizuki-n-2 <mizukizhangze@gmail.com>
2020-09-17 20:13:11 +0900

    2020/09/17

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <h1 class="header-logo">Station Search</h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  height: 70px;
+  line-height: 70px;
+  background: #000;
+}
+
+.header-logo {
+  font-size: 24px;
+  text-align: center;
+  color: #fff;
+  font-weight: bold;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home flex">
+    <div class="home-container">
+      <input type="text" v-model="stationName" placeholder="駅名を入力してください">
+      <button @click="showInfo">検索</button>
+      <p v-for="(data,index) in lists" :key="index">
+        駅情報{{index+1}}：{{data}}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      stationName: "",
+      lists: []
+    };
+  },
+  methods: {
+    async showInfo() {
+      this.lists = [];
+      let item = await axios.get(`http://api.ekispert.jp/v1/json/station?key=LE_zmTrsNKamrkQm&name=${this.stationName}`);
+      for (let i=0;i < item.data.ResultSet.Point.length;i++) {
+        this.lists.push(item.data.ResultSet.Point[i].Station.Name);
+      }
+      // console.log(item);
+      // console.log(this.lists);
+    }
   }
-}
+};
 </script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+
+.home-container {
+  margin-top: 100px;
+  background: #eee;
+  padding: 30px 60px;
+  border: 1px solid #000;
+  border-radius: 5px;
+}
+
+.home-container p {
+  margin: 20px 0;
+  padding-top: 10px;
+  border-bottom: 1px solid #ccc;
+}
+</style>
